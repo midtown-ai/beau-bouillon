d533bc43dcc4aa28cf7a0d73dcc7f172f0c2cffb
Emmanuel Mayssat <emayssat@Emmanuels-MacBook-Pro.local>
2023-03-21 14:07:05 -0700

    init

---
+++ scrape.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.example.com'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print(soup.prettify())
